c049abf73d76ed9edcf83da46db8b1091d636d24
Yuki-0307 <83302238+dun86@users.noreply.github.com>
2023-04-15 04:16:36 +0000

    First commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+  public static void main(String[] args) {
+    int count = 0; // 偶数の個数をカウントする変数
+    int sum = 0;   // 偶数の合計を計算する変数
+
+    for(int i=1; i<=10; i++){
+      if(i%2==0){
+        count++;  // 偶数の個数をカウント
+        sum += i; // 偶数の合計を計算
+      }
+    }
+
+    System.out.println("偶数の個数は" + count + "個です。");
+    System.out.println("偶数の合計は" + sum + "です。");
+    
+    
+    for(int i = 1; i <= 10; i++) {
+      System.out.print(i);
+      if(i != 10) {
+        System.out.print(",");
+      }
+    }
+  }
+}
---
+++ practice_1.java
@@ -0,0 +1,9 @@
+public class practice_1 {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            if(i%2==0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
